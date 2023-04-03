6e82490f3103f27576cd1afacb385d8e7a0c9ef2
rmc3408 <rmc3408@protonmail.com>
2023-04-03 12:50:11 -0400

    section 18 done

--- siecola/lib/event-product-stack.ts
+++ siecola/lib/event-product-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, RemovalPolicy, StackProps } from 'aws-cdk-lib';
+import { Stack, RemovalPolicy, StackProps, Duration } from 'aws-cdk-lib';
 import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
@@ -17,8 +17,25 @@ class EventProductsStack extends Stack {
       timeToLiveAttribute: "ttl",
       removalPolicy: RemovalPolicy.DESTROY,
       billingMode:  BillingMode.PROVISIONED,
-      readCapacity: 1,
       writeCapacity: 1,
+      readCapacity: 1
+    })
+
+    this.productsEventDatabase.autoScaleReadCapacity({
+      maxCapacity: 2,
+      minCapacity: 1
+    }).scaleOnUtilization({
+      targetUtilizationPercent: 50,
+      scaleInCooldown: Duration.seconds(60),
+      scaleOutCooldown: Duration.seconds(60)
+    })
+    this.productsEventDatabase.autoScaleWriteCapacity({
+      maxCapacity: 4,
+      minCapacity: 1
+    }).scaleOnUtilization({
+      targetUtilizationPercent: 30,
+      scaleInCooldown: Duration.seconds(60),
+      scaleOutCooldown: Duration.seconds(60)
     })
   }
 }
